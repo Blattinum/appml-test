7a80f928a50c6de63317a11b552ab8bf6b82c801
blattinum <collin.blatt@gmail.com>
2020-12-09 15:00:06 -0500

    ...

---
+++ assets/js/characters1.js
@@ -0,0 +1,51 @@
+﻿{"records":[
+{"name": "Harry Potter","species": "human","gender": "male","house": "Gryffindor","dateOfBirth": "31-07-1980","yearOfBirth": 1980,"ancestry": "half-blood","eyeColour": "green","hairColour": "black","wand": {"wood": "holly","core": "phoenix feather","length": 11},"patronus": "stag","hogwartsStudent": true,"hogwartsStaff": false,"actor": "Daniel Radcliffe","alive": true,"image": "assets/img/harry.jpg"},
+{"name": "Ginny Weasley","species": "human","gender": "female","house": "Gryffindor","dateOfBirth": "11-08-1981","yearOfBirth": 1981,"ancestry": "pure-blood","eyeColour": "brown","hairColour": "red","wand": {"wood": "yew","core": "","length": ""},"patronus": "horse","hogwartsStudent": true,"hogwartsStaff": false,"actor": "Bonnie Wright","alive": true,"image": "assets/img/ginny.jpg"},
+{
+    "name": "Sirius Black",
+    "species": "human",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "03-11-1959",
+    "yearOfBirth": 1959,
+    "ancestry": "pure-blood",
+    "eyeColour": "grey",
+    "hairColour": "black",
+    "wand": {
+      "wood": "",
+      "core": "",
+      "length": ""
+    },
+    "patronus": "hare",
+    "hogwartsStudent": false,
+    "hogwartsStaff": false,
+    "actor": "Gary Oldman",
+    "alive": false,
+    "image": "assets/img/sirius.JPG"
+  },
+{"name": "Rubeus Hagrid","species": "half-giant","gender": "male","house": "Gryffindor","dateOfBirth": "06-12-1928","yearOfBirth": 1928,"ancestry": "half-blood","eyeColour": "black","hairColour": "black"},
+{
+    "name": "Remus Lupin",
+    "species": "werewolf",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "10-03-1960",
+    "yearOfBirth": 1960,
+    "ancestry": "half-blood",
+    "eyeColour": "green",
+    "hairColour": "brown",
+    "wand": {
+      "wood": "cypress",
+      "core": "unicorn tail-hair",
+      "length": 10.25
+    },
+    "patronus": "wolf",
+    "hogwartsStudent": false,
+    "hogwartsStaff": true,
+    "actor": "David Thewlis",
+    "alive": false,
+    "image": "assets/img/lupin.jpg"
+  },
+{"name": "Ron Weasley","species": "human","gender": "male","house": "Gryffindor","dateOfBirth": "01-03-1980","yearOfBirth": 1980,"ancestry": "pure-blood","eyeColour": "blue","hairColour": "red","wand": {"wood": "willow","core": "unicorn tail-hair","length": 14},"patronus": "Jack Russell terrier","hogwartsStudent": true,"hogwartsStaff": false,"actor": "Rupert Grint","alive": true,"image": "assets/img/ron.jpg"}
+]
+} 
