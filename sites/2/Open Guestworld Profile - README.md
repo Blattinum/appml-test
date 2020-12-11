d8703478a936c75183c7f33e95355aecc1bf6fe3
blattinum <collin.blatt@gmail.com>
2020-12-11 12:09:46 -0500

    ...

---
+++ sites/2/Open Guestworld Profile - README.md
@@ -0,0 +1,14 @@
+﻿# Open Guestworld Profile
+
+### Process
+
+**sqlcmd_ogp.ps1** is a PowerShell script that is scheduled (Windows Task Scheduler) to *run every 15 mins* (e.g. 3pm, 3:15pm, 3:30pm, 3:45pm, 4pm, etc.) 24/7.
+
+This script exports the 211 card numbers and their corresponding Guestworld IDs into a JSON file called '**exportedIDs.json**'.
+
+***
+
+The webpage is located at http://172.16.10.23/OpenGuestworldProfile/  (http://ogp.meltrotter.org also redirects to the same place).
+
+With a barcode reader attached to the computer, the inputted number goes into the search box and will open up the guest's Guestworld profile. This is done using JavaScript. The JavaScript looks for the inputted 211 card number's corresponding Guestworld ID. Then the returned Guestworld ID is appended the http://172.16.10.23:5006/Guest/ServiceHistory/ URL, which pulls up their Guestworld profile.
+
---
+++ sites/2/script.js
@@ -0,0 +1,63 @@
+
+var verID = '?' + Date.now();
+var gwURL = "http://172.16.10.23:5006/Guest/ServiceHistory/";
+
+window.onload = function() {
+    document.querySelector('#main-script').src += verID;
+};
+
+window.onload = function msieversion() {
+
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+
+  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+  {
+      document.querySelector('#main-field').className = "display-none";
+      alert('This webpage does not support Internet Explorer. Please use a modern browser such as Chrome or Edge.');
+      window.location.href = "about:blank";
+  }
+
+  return false;
+}
+
+var jsonData = {};
+  $.ajax({
+      url: "./assets/json/exportedIDs.json" + verID,
+      async: false,
+      dataType: 'json',
+      success: function (data) {
+          jsonData = data;
+      }
+  });
+
+  console.log(jsonData);
+
+/* Search and launch pop up */
+function doSearch() {
+
+  var barcodeID = document.querySelector('#inputSearch').value;
+
+      $.each(jsonData, function (i, v) {
+        if (v.b == barcodeID) {
+          guestID = v.g;
+        }
+        /*else {
+          guestID = 0;
+        }*/
+        return;
+      });
+      var params = [
+        'height=' + screen.height,
+        'width=' + screen.width,
+        'fullscreen=yes', // only works in IE, but here for completeness
+    
+      ].join(',');
+      // and any other options from
+      // https://developer.mozilla.org/en/DOM/window.open
+
+      var popup = window.open(gwURL + guestID, "popup_window", params);
+      popup.moveTo(0, 0);
+};
+
+
