f141b24d6baf244283a4ef9d5a235ab3e23bf392
Collin Blatt <CollinBlatt@meltrotter.org>
2021-01-04 11:59:14 -0500

    ...

---
+++ sites/6/assets/js/ID/675.js
@@ -0,0 +1,6 @@
+﻿{"records":[
+{"ServiceType":"'Check Out'","Details":"AUTO-CHECKOUT","ProvidedBy":"MELTROTTER\\PurpleEastIntake", "ServiceTime":"1-4-2021 10:10:33 AM"},
+{"ServiceType":"'Check In'","Details":"250 Ionia","ProvidedBy":"Umbrella", "ServiceTime":"12-23-2020 09:17:00 AM"},
+{"ServiceType":"'Check Out'","Details":"AUTO-CHECKOUT","ProvidedBy":"MELTROTTER\\autovcm", "ServiceTime":"4-8-2019 10:28:01 AM"}
+]
+} 
