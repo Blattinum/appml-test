a3687cdc2086b79e3640552ef4ec36fefa252bdb
Collin Blatt <CollinBlatt@meltrotter.org>
2021-01-05 13:11:08 -0500

    ...

---
+++ sites/7/assets/js/printers.js
@@ -0,0 +1,5 @@
+﻿{"records":[
+    {"Make":"LEXMARK","Model":"XM3250","Serial":"7018925305KZX","MAC":"788C7760818b","IP":"172.16.21.215","Name":"MTM-GSS Basement Office"},
+    {"Make":"LEXMARK","Model":"M3250","Serial":"460092530XKTR","MAC":"788C77006BF2","IP":"172.16.12.12","Name":"MTM-IT Office"}
+]
+} 
